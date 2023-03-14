3e6fb760a12a63072382c182d75573ccd05f3ea0
jorgehernandezch <jorge@galpha.co>
2023-03-14 09:23:14 -0300

    Home and Login Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/Button";
+
+export function Home(){
+  return(
+    <>
+      <h1 className="text-lg text-cyan-400">Home</h1>
+      <Button />
+    </>
+
+  )
+}
