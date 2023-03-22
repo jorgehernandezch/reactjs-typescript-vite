038720c5f8b780f0b325eb8b1f1f106a37f95836
jorgehernandezch <jorge@galpha.co>
2023-03-22 10:37:29 -0300

    Footer Component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer(){
+  return(
+    <div className="min-h-[56px] h-14 max-h-[56px] w-full p-3 border-[1px] border-slate-500 ">
+      <div className="flex flex-row justify-between items-center gap-2 w-full h-full max-w-[1200px] mx-auto">
+        <div className="flex items-center px-2 border-[1px] border-slate-300 h-full flex-1">
+          Copyright
+        </div>
+        <div className="flex items-center px-2 border-[1px] border-slate-300 h-full w-1/5 max-w-[20%] min-w-[20%]">
+          Nav Footer
+        </div>
+      </div>
+    </div>
+  )
+}
