49ac5a0461418a910dca329b48d1cd84f95e14ed
jorgehernandezch <jorge@galpha.co>
2023-03-22 10:37:47 -0300

    Routes Home and Profile

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
 import { Login } from './pages/Login';
 
 export function AppRoutes(){
@@ -7,6 +8,7 @@ export function AppRoutes(){
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
