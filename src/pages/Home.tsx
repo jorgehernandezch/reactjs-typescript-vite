a00929c21ff2ee9076523b6688a917bb5d2e074e
jorgehernandezch <jorge@galpha.co>
2023-03-22 10:37:58 -0300

    Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
-import { Header } from "../components/Header"
+import { Page } from "../components/Page";
 
 export function Home(){
   return(
-    <>
-      <Header/>
-    </>
-
+    <Page>
+      <div className="flex flex-row justify-between items-center gap-3 w-full max-w-[1200px] mx-auto">
+        <div className="w-3/4 max-w-[75%] min-w-[75%] border-[1px] border-slate-300 min-h-full">
+          Main
+        </div>
+        <div className="flex-1 border-[1px] border-slate-300 min-h-full">
+          Aside
+        </div>
+      </div>
+    </Page>
   )
 }
